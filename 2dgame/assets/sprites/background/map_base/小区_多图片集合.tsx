f999ad6cd0fa1b546c12cabc584ab13a4f19fb05
kings_yan <sjy20030208@126.com>
2025-11-05 16:45:50 +0800

    提交测试

---
+++ 2dgame/assets/sprites/background/map_base/小区_多图片集合.tsx
@@ -0,0 +1,46 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="小区_多图片集合" tilewidth="33" tileheight="47" tilecount="14" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="block_orange.png" width="31" height="31"/>
+ </tile>
+ <tile id="1">
+  <image source="block_red.png" width="31" height="32"/>
+ </tile>
+ <tile id="2">
+  <image source="box_wooden.png" width="32" height="32"/>
+ </tile>
+ <tile id="3">
+  <image source="box_wooden2.png" width="31" height="30"/>
+ </tile>
+ <tile id="4">
+  <image source="floor_iron.png" width="32" height="32"/>
+ </tile>
+ <tile id="5">
+  <image source="grass.png" width="32" height="32"/>
+ </tile>
+ <tile id="7">
+  <image source="house_blue.png" width="32" height="47"/>
+ </tile>
+ <tile id="8">
+  <image source="house_yellow.png" width="30" height="47"/>
+ </tile>
+ <tile id="9">
+  <image source="ironbox_blue.png" width="32" height="31"/>
+ </tile>
+ <tile id="10">
+  <image source="ironbox_lightblue.png" width="32" height="32"/>
+ </tile>
+ <tile id="11">
+  <image source="ironbox_yellowandblack.png" width="32" height="32"/>
+ </tile>
+ <tile id="12">
+  <image source="roadblock_red.png" width="31" height="32"/>
+ </tile>
+ <tile id="6">
+  <image source="grass_green.png" width="33" height="45"/>
+ </tile>
+ <tile id="13">
+  <image source="tree.png" width="32" height="46"/>
+ </tile>
+</tileset>
